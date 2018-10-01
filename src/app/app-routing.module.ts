253b62791d36e0b4dd27dd1281c84a756296ca7b
Bruno João <bruno@tzadi.com>
2018-09-30 21:33:51 -0300

    routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { LocalComponent } from './pages/local/local.component';
+import { ProgramacaoComponent } from './pages/programacao/programacao.component';
+import { OrientacoesComponent } from './pages/orientacoes/orientacoes.component';
+import { PresentesComponent } from './pages/presentes/presentes.component';
+
+const AppRoutes = [
+  { path: '', component: HomeComponent },
+  { path: 'localedata', component: LocalComponent }
+  { path: 'programacao', component: ProgramacaoComponent }
+  { path: 'orientacoes', component: OrientacoesComponent }
+  { path: 'presentes', component: PresentesComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(AppRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LocalComponent } from './pages/local/local.component';
+import { OrientacoesComponent } from './pages/orientacoes/orientacoes.component';
+import { PresentesComponent } from './pages/presentes/presentes.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProgramacaoComponent } from './pages/programacao/programacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LocalComponent,
+    OrientacoesComponent,
+    PresentesComponent,
+    ProgramacaoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
