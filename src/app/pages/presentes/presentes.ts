e9c09910ad527c3a3f1a405e810c26b18d3f113b
Bruno Joao <bruno.joao@decoracontent.com>
2018-10-08 15:01:23 -0300

    remove secadore add address

--- src/app/pages/presentes/presentes.ts
+++ src/app/pages/presentes/presentes.ts
@@ -2,7 +2,7 @@ export const PRESENTES = [
   { nome: 'Sofá 3 lugares retrátil', marcas: '', cor: 'Cinza escuro, Marron, Bege escuro' },
   { nome: 'Microondas 30+ litros', marcas: '', cor: 'Preto, Branco ou Prata' },
   { nome: 'Máquina de lavar 10kg+', marcas: 'Brastemp, Consul ou Electrolux', cor: '' },
-  { nome: 'Secadora de roupas 8kg+', marcas: '', cor: '' },
+  // { nome: 'Secadora de roupas 8kg+', marcas: '', cor: '' },
   { nome: 'Forno elétrico 30+ litros', marcas: '', cor: '' },
   { nome: 'Roupão de banho M feminino', marcas: '', cor: '' },
   { nome: 'Roupão de banho G masculino', marcas: '', cor: '' },
